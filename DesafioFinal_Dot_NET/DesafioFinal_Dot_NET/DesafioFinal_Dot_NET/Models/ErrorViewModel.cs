5c3d595d337c96f46e35e466c22bb59e3a71a18b
Wagner Costa <wagnercosta2200@gmail.com>
2022-07-09 21:25:46 -0300

    gerenciamento Contato(Projeto Final)

--- DesafioFinal_Dot_NET/DesafioFinal_Dot_NET/DesafioFinal_Dot_NET/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace DesafioFinal_Dot_NET.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
---
+++ DesafioFinal_Dot_NET/GerenciamentoContato/GerenciamentoContato/Data/Contexto.cs
@@ -0,0 +1,15 @@
+﻿using GerenciamentoContato.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace GerenciamentoContato.Data
+{
+    public class Contexto: DbContext
+     {
+        public Contexto(DbContextOptions<Contexto> options) : base(options)
+        {
+
+        }
+        public DbSet<Contato> Contato { get; set; }
+
+    }
+}
